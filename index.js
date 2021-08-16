56536871eca278b348529462b52bc4bdb5efa984
Chervin Tanilon <chervintanilon@Jomars-Mac-mini.local>
2021-08-16 18:01:22 +0800

    modify root route

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express");
-// const redis = require("redis");
+const redis = require("redis");
 const port = process.env.PORT || 8080;
 const app = express();
-// const client = redis.createClient({
-//   host: 'redis-server',
-// });
+const client = redis.createClient({
+  host: 'redis-server',
+});
 
-// client.set('visits',0);
+client.set('visits',0);
 
 app.get("/", (req, res) => {
-  // process.exit(0);
-  // client.get('visits', (err, visits) => {
-  //   res.send('Number of visits is ' + visits);
-  //   client.set('visits', parseInt(visits) + 1);
-  // })
-  res.send("<h1>Im live from containers2222223</h1>")
+  process.exit(0);
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits is ' + visits);
+    client.set('visits', parseInt(visits) + 1);
+  })
+  // res.send("<h1>Im live from containers2222223</h1>")
 });
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
